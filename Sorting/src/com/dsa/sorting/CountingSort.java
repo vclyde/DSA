cf91cb2da55ba683afa542f6deb71a9e8ac77796
Clyde Velasquez <clydevelasquez@outlook.com>
2020-06-08 23:09:18 +0800

    Improve to accommodate array with negative numbers

--- Sorting/src/com/dsa/sorting/CountingSort.java
+++ Sorting/src/com/dsa/sorting/CountingSort.java
@@ -6,36 +6,51 @@ public class CountingSort {
 
     public static void main(String[] args) {
 
-        int[] n = {11, 7, 9, 1, 2, 23, 5, 1, 1, 0, 23};
+        int[] n = {11, 7, 9, 1, 2, 23, 5, 1, -1, -5, 0, -1};
         System.out.println("Before: " + Arrays.toString(n));
         countingSort(n);
-        System.out.println("After: " + Arrays.toString(n));
+        System.out.println("After : " + Arrays.toString(n));
     }
 
     // Counting sort for positive int
     public static void countingSort(int[] n) {
 
-        int max = n[0];
+        int max = n[0], min = 0;
         // Find the maximal number
         for (int i = 1; i < n.length; i++) {
             if (n[i] > max) {
                 max = n[i];
             }
+
+            if (n[i] < min) {
+                min = n[i];
+            }
         }
 
         // Create an array of counters
-        int[] count = new int[max + 1];
+        int[] pos = new int[max + 1];
+        int[] neg = new int[(min * -1) + 1];
 
-        // Count the frequencies of each number
+        // pos the frequencies of each number
         for (int value : n) {
-            count[value]++;
+            if (value >= 0) {
+                pos[value]++;
+            } else {
+                neg[value * -1]++;
+            }
         }
 
         int k = 0;
-        for (int i = 0; i < count.length; i++) {
+        // For negative numbers if there are any
+        for (int i = 0; i < neg.length; i++) {
+            for (int j = 0; j < neg[i]; j++) {
+                n[k++] = -i;
+            }
+        }
 
-            // Loop each count of number i
-            for (int j = 0; j < count[i]; j++) {
+        for (int i = 0; i < pos.length; i++) {
+            // Loop each pos of number i
+            for (int j = 0; j < pos[i]; j++) {
                 n[k++] = i;
             }
         }
