3791bae26ccf194d17409c0ec90a96df0f5730e8
Clyde Velasquez <clydevelasquez@outlook.com>
2020-12-22 15:47:51 +0800

    Implement a SinglyLinkedList

---
+++ src/main/java/com/vclyde/dsa/list/SinglyLinkedList.java
@@ -0,0 +1,145 @@
+package com.vclyde.dsa.list;
+
+import com.vclyde.dsa.interfaces.List;
+
+/**
+ *
+ * @author Clyde Velasquez
+ * @param <E> Generic Type
+ */
+public final class SinglyLinkedList<E> implements List<E> {
+
+	private static class Node<E> {
+
+		private E element;
+		private Node<E> next;
+
+		public Node(E element) {
+			this(element, null);
+		}
+
+		public Node(E element, Node<E> next) {
+			this.element = element;
+			this.next = next;
+		}
+	}
+
+	private Node<E> head;
+	private Node<E> tail;
+	private int count;
+
+	public SinglyLinkedList() {
+		head = tail = null;
+		count = 0;
+	}
+
+	@Override
+	public int size() {
+		return count;
+	}
+
+	@Override
+	public void clear() {
+		head = tail = null; // Null
+		count = 0; // Reset count
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return count == 0;
+	}
+
+	@Override
+	public E first() {
+		if (isEmpty()) {
+			return null;
+		}
+		return head.element;
+	}
+
+	@Override
+	public E last() {
+		if (isEmpty()) {
+			return null;
+		}
+		return tail.element;
+	}
+
+	@Override
+	public void addFirst(E e) {
+		head = new Node<>(e, head);
+
+		if (isEmpty()) { // If list is empty,
+			tail = head; // then set new node as head and tail
+		}
+
+		++count;
+	}
+
+	@Override
+	public void addLast(E e) {
+		Node<E> newNode = new Node<>(e);
+
+		if (isEmpty()) { // If list is empty,
+			head = newNode; // then set new node as head and tail
+		} else {
+			tail.next = newNode;
+		}
+
+		tail = newNode;
+		++count;
+	}
+
+	@Override
+	public E removeFirst() {
+		if (isEmpty()) {
+			return null;
+		}
+
+		E element = head.element;
+		head = head.next;
+		--count;
+
+		if (count == 0) {
+			tail = null;
+		}
+
+		return element;
+	}
+
+	@Override
+	public E removeLast() {
+		if (isEmpty()) {
+			return null;
+		}
+
+		E element = tail.element;
+		if (head == tail) {
+			head = tail = null;
+		} else {
+			Node<E> i = head;
+			while (i.next != tail) {
+				i = i.next;
+			}
+			tail = i;
+			tail.next = null;
+		}
+		--count;
+		return element;
+
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("{");
+		for (Node<E> i = head; i != null; i = i.next) {
+			sb.append(i.element);
+			if (i.next != null) {
+				sb.append(", ");
+			}
+		}
+		sb.append("}");
+		return sb.toString();
+	}
+}
---
+++ src/test/java/com/vclyde/dsa/list/SinglyLinkedListTest.java
@@ -0,0 +1,64 @@
+package com.vclyde.dsa.list;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author Clyde Velasquez
+ */
+public class SinglyLinkedListTest {
+
+	@Test
+	public void linkedListTest() {
+		SinglyLinkedList<Integer> intList = new SinglyLinkedList<>();
+
+		Assert.assertEquals(0, intList.size());
+		Assert.assertNull(intList.first());
+		Assert.assertNull(intList.last());
+
+		Assert.assertNull(intList.removeFirst());
+		Assert.assertNull(intList.removeLast());
+
+		intList.addFirst(2020);
+		Assert.assertEquals(1, intList.size());
+		Assert.assertNotNull(intList.first());
+		Assert.assertNotNull(intList.last());
+
+		Integer first = intList.first();
+		Integer last = intList.last();
+		Assert.assertEquals(2020, first.intValue());
+		Assert.assertEquals(2020, last.intValue());
+
+		intList.addLast(50);
+		System.out.println(intList);
+		last = intList.last();
+		assert last != null;
+		Assert.assertEquals(50, last.intValue());
+		Assert.assertEquals(2, intList.size());
+
+		intList.addFirst(21);
+		System.out.println(intList);
+		first = intList.first();
+		assert first != null;
+		Assert.assertEquals(21, first.intValue());
+		Assert.assertEquals(3, intList.size());
+
+		Integer remove = intList.removeFirst();
+		assert remove != null;
+		Assert.assertEquals(21, remove.intValue());
+		Assert.assertEquals(2, intList.size());
+		System.out.println(intList);
+
+		remove = intList.removeLast();
+		assert remove != null;
+		Assert.assertEquals(50, remove.intValue());
+		System.out.println(intList);
+
+		intList.clear();
+		Assert.assertEquals(0, intList.size());
+		Assert.assertNull(intList.first());
+		Assert.assertNull(intList.last());
+	}
+	
+}
