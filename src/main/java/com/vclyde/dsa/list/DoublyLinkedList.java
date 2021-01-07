9adbbd07235ab5f3f523473f71a56c3ef3b29c8e
Clyde Velasquez <clydevelasquez@outlook.com>
2021-01-07 10:37:36 +0800

    Implement additional methods from the List interface

--- src/main/java/com/vclyde/dsa/list/DoublyLinkedList.java
+++ src/main/java/com/vclyde/dsa/list/DoublyLinkedList.java
@@ -84,6 +84,94 @@ public class DoublyLinkedList<E> implements List<E> {
 		return count == 0;
 	}
 
+	@Override
+	public void add(E e) {
+		addLast(e);
+	}
+
+	@Override
+	public E get(int index) {
+		if (index >= 0 && index < count) {
+			Node<E> current = this.header.next;
+
+			for (int i = 0; i < index; i++) {
+				current = current.next;
+			}
+
+			return current.element;
+		} else {
+			throw new IndexOutOfBoundsException("Index is out of bounds! List size is " + count);
+		}
+	}
+
+	@Override
+	public void set(int index, E e) {
+		if (index >= 0 && index < count) {
+			Node<E> current = this.header.next;
+			for (int i = 0; i < index; i++) {
+				current = current.next;
+			}
+			current.element = e;
+		} else {
+			throw new IndexOutOfBoundsException("Index is out of bounds! List size is " + count);
+		}
+	}
+
+	@Override
+	public void add(int index, E e) {
+		if (index >= 0 && index <= count) {
+			Node<E> current = this.header.next;
+			for (int i = 0; i < index; i++) {
+				current = current.next;
+			}
+			addBetween(e, current.prev, current);
+		} else {
+			throw new IndexOutOfBoundsException("Index is out of bounds! List size is " + count);
+		}
+	}
+
+	@Override
+	public E remove(int index) {
+		if (index >= 0 && index < count) {
+			Node<E> current = this.header.next;
+			for (int i = 0; i < index; i++) {
+				current = current.next;
+			}
+			remove(current);
+
+			return current.element;
+		} else {
+			throw new IndexOutOfBoundsException("Index is out of bounds! List size is " + count);
+		}
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("{");
+
+		for (Node<E> current = this.header.next; current != this.trailer; current = current.next) {
+			sb.append(current.element);
+			if (current.next != this.trailer) {
+				sb.append(", ");
+			}
+		}
+
+		sb.append("}");
+
+		// In reverse order
+//		sb.append("\n");
+//		sb.append("{");
+//		for (Node<E> current = this.trailer.prev; current != this.header; current = current.prev) {
+//			sb.append(current.element);
+//			if (current.prev != this.header) {
+//				sb.append(", ");
+//			}
+//		}
+//		sb.append("}");
+		return sb.toString();
+	}
+
 	private void addBetween(E e, Node<E> predecessor, Node<E> successor) {
 
 		Node<E> newest = new Node<>(e, predecessor, successor);
@@ -105,44 +193,6 @@ public class DoublyLinkedList<E> implements List<E> {
 		--count;
 
 		return node.element;
-	}
-
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("{");
-		Node<E> i = this.header.next;
-		while (i != this.trailer) {
-			sb.append(i.element);
-
-			i = i.next;
-			if (i != this.trailer) {
-				sb.append(", ");
-			}
-		}
-		sb.append("}");
-		return sb.toString();
-	}
-
-	/**
-	 * Display in reverse order
-	 *
-	 * @return List as String in reverse order
-	 */
-	public String reverse() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("{");
-		Node<E> i = this.trailer.prev;
-		while (i != this.header) {
-			sb.append(i.element);
-
-			i = i.prev;
-			if (i != this.header) {
-				sb.append(", ");
-			}
-		}
-		sb.append("}");
-		return sb.toString();
 	}
 
 	private static class Node<E> {
--- src/test/java/com/vclyde/dsa/list/DoublyLinkedListTest.java
+++ src/test/java/com/vclyde/dsa/list/DoublyLinkedListTest.java
@@ -3,6 +3,7 @@ package com.vclyde.dsa.list;
 import org.junit.Assert;
 import org.junit.Test;
 
+
 /**
  * @author Clyde Velasquez
  */
@@ -10,25 +11,54 @@ public class DoublyLinkedListTest {
 
 	@Test
 	public void test() {
-		DoublyLinkedList<String> dll = new DoublyLinkedList<>();
-		Assert.assertEquals(0, dll.size());
-
-		dll.addFirst("Hello");
-		dll.addLast("World");
-		Assert.assertEquals(2, dll.size());
-		Assert.assertEquals("Hello", dll.first());
-		Assert.assertEquals("World", dll.last());
-		System.out.println(dll);
-
-		dll.addLast("Welcome");
-		Assert.assertEquals(3, dll.size());
-		Assert.assertEquals("Welcome", dll.removeLast());
-		Assert.assertEquals(2, dll.size());
-		Assert.assertEquals("Hello", dll.removeFirst());
-		Assert.assertEquals(1, dll.size());
-		System.out.println(dll);
-
-		dll.clear();
-		Assert.assertEquals("{}", dll.toString());
+		DoublyLinkedList<Character> list = new DoublyLinkedList<>();
+		Assert.assertTrue(list.isEmpty());
+		
+		// Test add first
+		list.addFirst('A');
+		list.addFirst('B');
+		list.addFirst('C');
+		Assert.assertFalse(list.isEmpty());
+		Assert.assertNotNull(list.first());
+		Assert.assertNotNull(list.last());
+		Assert.assertEquals(Character.valueOf('C'), list.first());
+		Assert.assertEquals(Character.valueOf('A'), list.last());
+		Assert.assertEquals(3, list.size());
+		
+		// Test add last
+		list.addLast('D');
+		list.addLast('F');
+		list.addLast('G');
+		list.addLast('H');
+		Assert.assertEquals(7, list.size());
+		Assert.assertEquals(Character.valueOf('H'), list.last());
+		
+		// Test remove first
+		Assert.assertEquals(Character.valueOf('C'), list.removeFirst());
+		Assert.assertEquals(6, list.size());
+		
+		// Test remove last
+		Assert.assertEquals(Character.valueOf('H'), list.removeLast());
+		Assert.assertEquals(5, list.size());
+		
+		// Test get
+		Assert.assertEquals(Character.valueOf('A'), list.get(1));
+		
+		// Test remove
+		Assert.assertEquals(Character.valueOf('A'), list.remove(1));
+		
+		// Test set
+		list.set(1, 'E');
+		Assert.assertEquals(Character.valueOf('E'), list.get(1));
+		
+		// Test add
+		list.add(0, 'z');
+		Assert.assertEquals(Character.valueOf('z'), list.get(0));
+		
+		// Clear
+		list.clear();
+		Assert.assertTrue(list.isEmpty());
+		Assert.assertNull(list.first());
+		Assert.assertNull(list.last());		
 	}
 }
